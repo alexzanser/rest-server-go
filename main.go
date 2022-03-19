b310b29d5b7243817ab42a7d01a43eb72db31001
tolkachev <alexzanser@mail.ru>
2022-03-19 23:12:50 +0300

    gracefully shutdown doesn't work

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/alexzanser/rest-server-go.git/server"
 	"github.com/go-chi/chi/v5"
@@ -11,18 +14,41 @@ import (
 
 
 func main() {
+	//Context will be used in other processes
+	_, cancel := context.WithCancel(context.Background())
+
 	server := server.NewTaskServer()
 	r := chi.NewRouter()
-	
-	r.Get("/task/", server.GetAllTasksHandler)
+
 	r.Post("/task/", server.CreateTaskHandler)
-	r.Delete("/task/{id}", server.DeleteTaskHandler)
+	r.Get("/task/", server.GetAllTasksHandler)
 	r.Get("/task/{id}", server.GetTaskHandler)
+	r.Delete("/task/{id}", server.DeleteTaskHandler)
 	r.Get("/tag/{tagname}", server.TagHandler)
 	r.Get("/due/{yy}/{mm}/{dd}", server.DueHandler)
 
 	log.Fatal(http.ListenAndServe("localhost:"+os.Getenv("SERVERPORT"), r))
 
+
+	//Gracefull shutdown
+	sigquit := make(chan os.Signal, 1)
+	signal.Ignore(syscall.SIGHUP, syscall.SIGPIPE)
+	signal.Notify(sigquit, syscall.SIGINT, syscall.SIGTERM)
+	stopAppCh := make(chan struct{})
+
+	go func() {
+		log.Println("Captured signal: ", <- sigquit)
+		log.Println("Gracefully shutting down server ...")
+
+		cancel()
+
+		if err := server.Shutdown(context.Background()); err != nil {
+			log.Println("Can't shutdown main server: ", err.Error())
+		}
+		stopAppCh <- struct{}{}
+	}()
+
+	<- stopAppCh
 }
 
 // func main() {
--- server/server.go
+++ server/server.go
@@ -14,6 +14,7 @@ import (
 )
 
 type taskServer struct {
+	http.Server
 	store *taskstore.TaskStore
 }
 
